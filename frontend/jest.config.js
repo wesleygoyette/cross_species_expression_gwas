cae01edbeaf519e48f7476d7d2648e7e3a01b27a
wesleygoyette <wesley.goyette@gmail.com>
2025-10-07 15:18:08 -0500

    Improved demo

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  moduleNameMapping: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.(js|jsx|ts|tsx)',
-    '<rootDir>/src/**/*.(test|spec).(js|jsx|ts|tsx)'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.(js|jsx|ts|tsx)',
-    '!src/**/*.d.ts',
-    '!src/main.tsx',
-    '!src/test/**',
-  ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
-      presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        ['@babel/preset-react', { runtime: 'automatic' }],
-        '@babel/preset-typescript',
-      ],
-    }],
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+    moduleNameMapping: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.(js|jsx|ts|tsx)',
+        '<rootDir>/src/**/*.(test|spec).(js|jsx|ts|tsx)'
+    ],
+    collectCoverageFrom: [
+        'src/**/*.(js|jsx|ts|tsx)',
+        '!src/**/*.d.ts',
+        '!src/main.tsx',
+        '!src/test/**',
+    ],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+            presets: [
+                ['@babel/preset-env', { targets: { node: 'current' } }],
+                ['@babel/preset-react', { runtime: 'automatic' }],
+                '@babel/preset-typescript',
+            ],
+        }],
+    },
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
 };
